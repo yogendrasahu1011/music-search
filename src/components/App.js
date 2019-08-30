60b3a5704573d5ebd94a336a1de4b26eb986a220
yogendra <yogendrasahu1011@gmail.com>
2019-08-30 02:34:53 -0700

    music

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Artist from './Artist';
+import Tracks from './Tracks';
+import Search from './Search';
+console.log('here');
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+class App extends Component {
+    state = {artist: null, tracks:[]};
+   
+    searchArtist = artistQuery =>{
+       
+        fetch(`${API_ADDRESS}/artist/${artistQuery}`)
+        .then(response=>response.json())
+        .then(json =>{
+            
+            if(json.artists.total >0)
+            {
+                const artist = json.artists.items[0];
+           
+                this.setState({artist});
+                fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                .then(response=>response.json())
+                .then(json=>this.setState({tracks:json.tracks}))
+                .catch(error =>alert(error.message));
+                
+            }
+        })
+    }
+  
+    render(){
+       console.log(this.state);
+        return (
+            <div>
+              <h2>Music Search Engine</h2>
+             <Search searchArtist={this.searchArtist}/>
+              <Artist artist = {this.state.artist}/>
+              <hr/>
+              <Tracks tracks={this.state.tracks}/>
+            </div>
+    
+            
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+import './index.css';
+
+console.log('idex');
+
+ReactDOM.render(
+<App/>,
+document.getElementById('root')
+);
